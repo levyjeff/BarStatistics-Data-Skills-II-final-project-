bf7e01cd55ca5de730c9f995665fd219aa09dfbd
Beth Karp <bethkarp@uchicago.edu>
2020-12-05 19:37:30 -0600

    Race data added
    Need to move on to visualizations

--- Final_Project.py
+++ Final_Project.py
@@ -4,12 +4,17 @@ Initial housekeeping
 
 import os
 import pandas as pd
-import numpy as np
 import us
+
 import requests
 from bs4 import BeautifulSoup
 import tabula
+import PyPDF2
+import spacy
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
 
+nlp = spacy.load("en_core_web_sm")
 path = r'C:\Users\bethk\Desktop\Data_Skills_II\BarStatistics'
 os.chdir(path)
 headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'}
@@ -318,6 +323,101 @@ def edrates_df():
     return(ed)
 
 '''
+Race dataframes
+'''
+
+# Get race names by key from census PDF
+def get_statement(url):
+    if ('sc-est2019-alldata6.pdf' in os.getcwd()):
+        pass
+    else:
+        fname = url.split('/')[-1]
+        response = requests.get(url)
+        with open(os.path.join(path, fname), 'wb') as ofile:
+            ofile.write(response.content)
+def parse_pdf(a_pdf):
+    a_pdf = path + '\\' + a_pdf
+    fname = a_pdf.split('/')[-1]
+    with open(os.path.join(path, fname), 'rb') as ifile:
+        pdf_reader = PyPDF2.PdfFileReader(ifile)
+        pages = [pdf_reader.getPage(p) for p in range(1,2)]
+        pages = [p.extractText() for p in pages]
+    return pages
+
+get_statement('https://www2.census.gov/programs-surveys/popest/technical-documentation/file-layouts/2010-2019/sc-est2019-alldata6.pdf')
+codes = str(parse_pdf('sc-est2019-alldata6.pdf')).split('\\n')
+
+racewords = ['White', 'Black', 'Indian', 'Asian', 'Hawaiian', 'races']
+hispwords = ['Hispanic']
+
+def get_races(race_set):
+    racecodes = []
+    for item in codes:
+        for race in race_set:
+            if (race in item) & ('=' in item):
+                racecodes.append(item)
+    return(racecodes)
+
+def strip_whitespace(race_set):
+    stripped = [item.strip() for item in race_set]
+    return(stripped)
+
+def make_race_dict(race_set):
+    keys = []
+    vals = []
+    race_list = strip_whitespace(get_races(race_set))
+    for item in race_list:
+        first = int(item.split('=')[0].strip())
+        second = item.split('=')[1].strip()
+        keys.append(first)
+        vals.append(second)
+    output_dict = dict(zip(keys, vals))
+    return(output_dict)
+
+hisp = make_race_dict(hispwords)
+nonhisp = make_race_dict(racewords)
+nonhisp[1] = 'White'
+nonhisp[2] = 'Black'
+nonhisp[3] = 'Native'
+nonhisp[4] = 'Asian'
+nonhisp[5] = 'Pacific'
+nonhisp[6] = 'Multiracial'
+
+def race_df(): # Imports race data by state
+    if os.path.isfile(path+f'\\race.csv') == True:
+        racedata = pd.read_csv(path+f'\\race.csv')
+    else:
+        racedata = pd.read_csv('https://www2.census.gov/programs-surveys/popest/tables/2010-2019/state/asrh/sc-est2019-alldata6.csv')
+        racedata.to_csv('race.csv')
+# Remove region codes and aggregates
+    racedata = racedata[racedata['ORIGIN'] != 0]
+    racedata = racedata[racedata['SEX'] != 0]
+    racedata = racedata[['NAME', 'ORIGIN', 'RACE', 'POPESTIMATE2015', 'POPESTIMATE2016', 'POPESTIMATE2017', 'POPESTIMATE2018']]
+    return(racedata)
+
+racedata = race_df()
+
+# Map in race names
+racedata['Hisp'] = racedata['ORIGIN'].map(hisp)
+racedata['Nonhisp'] = racedata['RACE'].map(nonhisp)
+racedata['Race'] = racedata[['Nonhisp','Hisp']].apply(lambda x: ', '.join(x), axis=1)
+racedata = racedata[['NAME', 'POPESTIMATE2015', 'POPESTIMATE2016',
+       'POPESTIMATE2017', 'POPESTIMATE2018', 'Race']]
+
+# Make a dataframe with racial population estimates for each year
+def race_by_state(year):
+    r = racedata[['NAME', f'POPESTIMATE{year}', 'Race']]
+    r = r.groupby(['NAME', 'Race']).sum().reset_index()
+    r = r.pivot(index='NAME', columns='Race', values=f'POPESTIMATE{year}').reset_index()
+# Get race numbers as percentages then rename state column to match other dataframes
+    r['Total'] = r.sum(axis=1)
+    for col in r.columns[1:]:
+        r[col] = r[col]/r['Total']
+    del r['Total']
+    r = r.rename(columns={'NAME':'Jurisdiction'})
+    return(r)
+
+'''
 Merge dataframes
 '''
 
@@ -325,29 +425,43 @@ def merge_it(year):
     merged = pd.merge(passrates_df(year), povrates_df(year))
     merged = pd.merge(merged, get_disc(year))
     merged = pd.merge(merged, edrates_df())
+    merged = pd.merge(merged, race_by_state(year))
 # Add columns for unemployment rate for given year
     unemp = unemprates_df()
     unemp = unemp[unemp['Jurisdiction'].isin(
 merged['Jurisdiction'])].reset_index(drop=True)
-    merged['Unemployment_rate'] = unemp[f'Unemployment_rate_{year}']/100
+    merged['Unemployment_Rate'] = unemp[f'Unemployment_rate_{year}']/100
     merged['Year'] = year
     return(merged)
-
 # Note: this will drop US territories, leaving only states and Washington, D.C. It will also drop states for which no discipline data is avaiable through the ABA.
 
+def merge_dfs(start, end):
+    dfs = []
+    for year in range(start, end):
+        merged = merge_it(year)
+        dfs.append(merged)
+    all = pd.concat(dfs, axis=0).reset_index(drop=True)
+    return(all)
 
+alldata = merge_dfs(2015, 2019)
 
-df15 = merge_it(2015)
-df16 = merge_it(2016)
-df17 = merge_it(2017)
-df18 = merge_it(2018)
-df15
-df16
-df17
-df18
+'''
+Regressions
+'''
 
-z = pd.concat([df15, df16, df17, df18], axis=0).reset_index(drop=True)
-z
+x = alldata[['Pass_Rate', 'Poverty_Rate', 'Num_Lawyers', 'Unemployment_Rate', 'Year']]
+y = alldata['Complaints_Per_Lawyer']
+x = sm.add_constant(x)
+model = sm.OLS(y, x).fit()
+predictions = model.predict(x)
+model.summary()
+
+x = alldata[['Poverty_Rate', 'BA', 'Unemployment_Rate', 'Year']]
+y = alldata['Pass_Rate']
+x = sm.add_constant(x)
+model = sm.OLS(y, x).fit()
+predictions = model.predict(x)
+model.summary()
 
 '''
 Rudimentary plotting
@@ -356,24 +470,7 @@ Rudimentary plotting
 df15 = df15.sort_values('Pass_Rate')
 df16 = df16.sort_values('Pass_Rate')
 
-import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 ax.plot(df15['Jurisdiction'], df15['Pass_Rate'])
 ax.plot(df15['Jurisdiction'], df15['Poverty_Rate'])
 
-import statsmodels.api as sm
-
-x = z['Pass_Rate']
-y = z['Complaints_Per_Lawyer']
-x = sm.add_constant(x)
-model = sm.OLS(y, x).fit()
-predictions = model.predict(x)
-model.summary()
-
-
-x = z[['Poverty_Rate', 'Num_Lawyers', 'Takers']]
-y = z['Pass_Rate']
-x = sm.add_constant(x)
-model = sm.OLS(y, x).fit()
-predictions = model.predict(x)
-model.summary()
--- exp.py
+++ exp.py
@@ -1,38 +1,142 @@
 '''
-Add some data
+Initial housekeeping
 '''
-import pandas as pd
+
 import os
+import pandas as pd
 
-# Unemployment rates
-if os.path.isfile(path+ r'\unemp_rates.csv') == True:
-    pass
-else:
-    unemp_rates = pd.read_excel('https://www.ers.usda.gov/webdocs/DataFiles/48747/Unemployment.xls?v=4751.8', header=7)
-    unemp_rates.to_csv('unemp_rates.csv')
+import PyPDF2
+import requests
+import spacy
+nlp = spacy.load("en_core_web_sm")
 
-unemp = pd.read_csv('unemp_rates.csv')[['area_name', 'Unemployment_rate_2015', 'Unemployment_rate_2016', 'Unemployment_rate_2017', 'Unemployment_rate_2018']]
-unemp = unemp[unemp['area_name'].isin(z['Jurisdiction'])].reset_index(drop=True)
+path = r'C:\Users\bethk\Desktop\Data_Skills_II\BarStatistics'
+os.chdir(path)
+
+'''
+Race data
+'''
+
+# Get race names by key from census PDF
+def get_statement(url):
+    if ('sc-est2019-alldata6.pdf' in os.getcwd()):
+        pass
+    else:
+        fname = url.split('/')[-1]
+        response = requests.get(url)
+        with open(os.path.join(path, fname), 'wb') as ofile:
+            ofile.write(response.content)
+def parse_pdf(a_pdf):
+    a_pdf = path + '\\' + a_pdf
+    fname = a_pdf.split('/')[-1]
+    with open(os.path.join(path, fname), 'rb') as ifile:
+        pdf_reader = PyPDF2.PdfFileReader(ifile)
+        pages = [pdf_reader.getPage(p) for p in range(1,2)]
+        pages = [p.extractText() for p in pages]
+    return pages
+
+get_statement('https://www2.census.gov/programs-surveys/popest/technical-documentation/file-layouts/2010-2019/sc-est2019-alldata6.pdf')
+codes = str(parse_pdf('sc-est2019-alldata6.pdf')).split('\\n')
+
+racewords = ['White', 'Black', 'Indian', 'Asian', 'Hawaiian', 'races']
+hispwords = ['Hispanic']
+
+def get_races(race_set):
+    racecodes = []
+    for item in codes:
+        for race in race_set:
+            if (race in item) & ('=' in item):
+                racecodes.append(item)
+    return(racecodes)
+
+def strip_whitespace(race_set):
+    stripped = [item.strip() for item in race_set]
+    return(stripped)
+
+def make_race_dict(race_set):
+    keys = []
+    vals = []
+    race_list = strip_whitespace(get_races(race_set))
+    for item in race_list:
+        first = int(item.split('=')[0].strip())
+        second = item.split('=')[1].strip()
+        keys.append(first)
+        vals.append(second)
+    output_dict = dict(zip(keys, vals))
+    return(output_dict)
+
+hisp = make_race_dict(hispwords)
+nonhisp = make_race_dict(racewords)
+nonhisp[1] = 'White'
+nonhisp[2] = 'Black'
+nonhisp[3] = 'Native'
+nonhisp[4] = 'Asian'
+nonhisp[5] = 'Pacific'
+nonhisp[6] = 'Multiracial'
+
+def race_df(): # Imports race data by state
+    if os.path.isfile(path+f'\\race.csv') == True:
+        racedata = pd.read_csv(path+f'\\race.csv')
+    else:
+        racedata = pd.read_csv('https://www2.census.gov/programs-surveys/popest/tables/2010-2019/state/asrh/sc-est2019-alldata6.csv')
+        racedata.to_csv('race.csv')
+# Remove region codes and aggregates
+    racedata = racedata[racedata['ORIGIN'] != 0]
+    racedata = racedata[racedata['SEX'] != 0]
+    racedata = racedata[['NAME', 'ORIGIN', 'RACE', 'POPESTIMATE2015', 'POPESTIMATE2016', 'POPESTIMATE2017', 'POPESTIMATE2018']]
+    return(racedata)
+
+racedata = race_df()
+
+# Map in race names
+racedata['Hisp'] = racedata['ORIGIN'].map(hisp)
+racedata['Nonhisp'] = racedata['RACE'].map(nonhisp)
+racedata['Race'] = racedata[['Nonhisp','Hisp']].apply(lambda x: ', '.join(x), axis=1)
+racedata = racedata[['NAME', 'POPESTIMATE2015', 'POPESTIMATE2016',
+       'POPESTIMATE2017', 'POPESTIMATE2018', 'Race']]
+
+# Make a dataframe with racial population estimates for each year
+
+def race_by_state(year):
+    r = racedata[['NAME', f'POPESTIMATE{year}', 'Race']]
+# Bring in totals
+    total_df = r.groupby('NAME').sum().reset_index()
+    total_df.rename(columns={f'POPESTIMATE{year}': 'Total'})
+    r = pd.merge(r, total_df)
 
 
-# Educational attainment
+    r['Total'] = r.groupby('NAME').sum().reset_index()
+    return(r)
 
-if os.path.isfile(path+ r'\ed_attainment.csv') == True:
-    pass
-else:
-    ed = pd.read_excel('https://www.ers.usda.gov/webdocs/DataFiles/48747/Education.xls?v=4751.8', header=4)
-    ed.to_csv('ed_attainment.csv')
-ed = pd.read_csv('ed_attainment.csv')
-ed = ed[ed['Area name'].isin(z['Jurisdiction'])].reset_index(drop=True)
-# Get rid of duplicate Washington, D.C.
-ed = ed.drop_duplicates(subset='Area name', inplace=False)
+    r = r.groupby(['NAME', 'Race']).sum().reset_index()
+    r = r.pivot(index='NAME', columns='Race', values=f'POPESTIMATE{year}').reset_index()
+#    r['Year'] = year
+    return(r)
 
-ed = ed[['Area name', 'Less than a high school diploma, 2014-18',
-       'High school diploma only, 2014-18',
-       "Some college or associate's degree, 2014-18",
-       "Bachelor's degree or higher, 2014-18",
-       'Percent of adults with less than a high school diploma, 2014-18',
-       'Percent of adults with a high school diploma only, 2014-18',
-       "Percent of adults completing some college or associate's degree, 2014-18",
-       "Percent of adults with a bachelor's degree or higher, 2014-18"]]
+y = y.rename(columns={'POPESTIMATE2017': 'Total'})
+z = pd.merge(x, y)
 
+x['Total'] = 0
+
+for num in range(len(x)):
+    for col in x.columns[1:]:
+        x[col] = (x[col])/(x.iloc[num].sum())
+
+
+    for col in x.columns[1:]:
+        print([col][num])
+
+        x[col][num] = (x[col][num])/(x.iloc[num].sum())
+
+def combine_years():
+    dfs = []
+    for year in range(2015, 2018):
+
+
+        def merge_dfs(start, end):
+    dfs = []
+    for year in range(start, end):
+        merged = merge_it(year)
+        dfs.append(merged)
+    all = pd.concat(dfs, axis=0).reset_index(drop=True)
+    return(all)
